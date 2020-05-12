f50535405ca1e4afae53e040e3df6a6ea5af2936
Julian Mora <jmora29@bancodebogota.com.co>
2020-05-12 10:07:45 -0500

    v1

---
+++ src/components/my-component/my-component.tsx
@@ -0,0 +1,43 @@
+import { Component, Prop, h, Element, Event, EventEmitter, State } from '@stencil/core';
+
+@Component({
+  tag: 'my-form',
+  styleUrl: 'my-component.css',
+  shadow: true
+})
+export class MyComponent {
+ 
+  @Prop() items: any[] = [{id: 1, name: 'PYTHON'}, {id: 2, name: 'JAVASCRIPT'}, {id: 3, name: 'JAVA'}];
+  
+  @Element() el: HTMLElement;
+  
+  @Event() valueChance:  EventEmitter;
+  
+  @State() activeMenu: boolean = false;
+  @State() value: any;
+  
+  render() {
+    return <div>
+        <form>
+		<p>Nombre:</p>
+		<input type="text" class="field"/> <br/>
+
+		<p>Correo electrónico:</p>
+		<input type="text" class="field"/> <br/>
+
+		<p>Asunto:</p>
+		<input type="text" class="field"/> <br/>
+
+
+
+		<p>Mensaje:</p>
+		<textarea class="field"></textarea> <br/>
+
+		<p class="center-content">
+			<input type="submit" class="btn btn-green" value="Enviar Datos"/>
+		</p>
+	</form>
+      </div>;
+  }
+
+}
---
+++ src/components/my-component/readme.md
@@ -0,0 +1,24 @@
+# my-component
+
+
+
+<!-- Auto Generated Below -->
+
+
+## Properties
+
+| Property | Attribute | Description | Type    | Default                                                                         |
+| -------- | --------- | ----------- | ------- | ------------------------------------------------------------------------------- |
+| `items`  | --        |             | `any[]` | `[{id: 1, name: 'PYTHON'}, {id: 2, name: 'JAVASCRIPT'}, {id: 3, name: 'JAVA'}]` |
+
+
+## Events
+
+| Event         | Description | Type               |
+| ------------- | ----------- | ------------------ |
+| `valueChance` |             | `CustomEvent<any>` |
+
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
