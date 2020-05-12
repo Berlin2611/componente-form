3820590a327ac19c1e77a0f04015d60df0b35d79
Julian Mora <jmora29@bancodebogota.com.co>
2020-05-12 16:04:18 -0500

    v3

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -1,5 +1,5 @@
 import { Component, Prop, h, Element, Event, EventEmitter, State } from '@stencil/core';
-
+import Swal from 'sweetalert2';
 @Component({
   tag: 'my-form',
   styleUrl: 'my-component.css',
@@ -18,21 +18,60 @@ export class MyComponent {
   @State() activeMenu: boolean = false;
   @State() value: any;
 
+
+
+  handleChange(event) {
+    this.value = event.target.value;
+  }
+  handleSubmit(e) {
+    e.preventDefault()
+    console.log(this.value);
+    // send data to our backend
+    const formData = new FormData(e.target);
+
+    fetch('https://httpbin.org/post', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: formData
+    })
+    .then(function(response) {
+        console.log('response =', response);
+        return response.json();
+    })
+    .then(function(data) {
+        console.log('data = ', data);  
+          Swal.fire({
+            title: '¡Excelente!',
+            text: 'El formulario se ha enviado',
+            icon: 'success',
+            confirmButtonText: 'Volver'
+          })
+    })
+    .catch(function(err) {
+        console.error(err);
+        Swal.fire({
+          title: '¡Error!',
+          text: 'El formulario no se ha enviado',
+          icon: 'error',
+          confirmButtonText: 'Volver'
+        })
+    });
+  }
+
   render() {
     return <div>
         <h1>{this.title1}</h1>
-        <form>
-          <p>Nombre:</p>
-          <input type="text" class="field"/> <br/>
+        <form onSubmit={(e) => this.handleSubmit(e)}>
+          <p>Nombres:</p>
+          <input name="nombress" type="text" class="field"/> <br/>
 
+          <p>Edad:</p>
+          <input name="edad" type="text" class="field"/> <br/>
+         
           <p>Correo electrónico:</p>
-          <input type="text" class="field"/> <br/>
-
-          <p>Asunto:</p>
-          <input type="text" class="field"/> <br/>
-
-          <p>Mensaje:</p>
-          <textarea class="field"></textarea> <br/>
+          <input name="correo" type="text" class="field"/> <br/>
 
           <p class="center-content">
             <input type="submit" class="btn btn-green" value="Enviar Datos"/>
